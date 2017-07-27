acf8ae948f0e3dc6666a64ca53b655797ed22a60
Yongwoo Lee <whylee259@gmail.com>
2017-07-27 20:16:19 +0900

    Converts a java class `TasksPersistenceContract` to kotlin.

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksPersistenceContract.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksPersistenceContract.kt
@@ -21,18 +21,19 @@ import android.provider.BaseColumns;
 /**
  * The contract used for the db to save the tasks locally.
  */
-public final class TasksPersistenceContract {
-
+class TasksPersistenceContract private constructor() {
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor.
-    private TasksPersistenceContract() {}
 
     /* Inner class that defines the table contents */
-    public static abstract class TaskEntry implements BaseColumns {
-        public static final String TABLE_NAME = "task";
-        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
-        public static final String COLUMN_NAME_TITLE = "title";
-        public static final String COLUMN_NAME_DESCRIPTION = "description";
-        public static final String COLUMN_NAME_COMPLETED = "completed";
+    abstract class TaskEntry : BaseColumns {
+        companion object {
+            const val _ID = BaseColumns._ID //Kotlin doesn't allow accessing static fields of super classes.
+            const val TABLE_NAME = "task"
+            const val COLUMN_NAME_ENTRY_ID = "entryid"
+            const val COLUMN_NAME_TITLE = "title"
+            const val COLUMN_NAME_DESCRIPTION = "description"
+            const val COLUMN_NAME_COMPLETED = "completed"
+        }
     }
 }
