6c68bfe56824e8ada049b2796320f141d5e0e791
Yongwoo Lee <whylee259@gmail.com>
2017-07-29 22:55:48 +0900

    Converts a java class `TaskItemNavigator` to kotlin.

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TaskItemNavigator.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TaskItemNavigator.kt
@@ -18,8 +18,10 @@ package com.example.android.architecture.blueprints.todoapp.tasks;
 
 /**
  * Defines the navigation actions that can be called from a list item in the task list.
+ *
+ * Converted to kotlin by whylee259@gmail.com
  */
-public interface TaskItemNavigator {
+interface TaskItemNavigator {
 
-    void openTaskDetails(String taskId);
+    fun openTaskDetails(taskId: String)
 }
