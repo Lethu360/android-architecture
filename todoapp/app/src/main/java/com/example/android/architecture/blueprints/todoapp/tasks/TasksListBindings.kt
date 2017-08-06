62bbb48cab357f65355342debd61160a61a23364
Yongwoo Lee <whylee259@gmail.com>
2017-08-06 15:29:23 +0900

    Converts a java class `TasksListBindings` to kotlin.

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksListBindings.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksListBindings.kt
@@ -14,27 +14,25 @@
  * limitations under the License.
  */
 
-package com.example.android.architecture.blueprints.todoapp.tasks;
+package com.example.android.architecture.blueprints.todoapp.tasks
 
-import android.databinding.BindingAdapter;
-import android.widget.ListView;
+import android.databinding.BindingAdapter
+import android.widget.ListView
 
-import com.example.android.architecture.blueprints.todoapp.data.Task;
+import com.example.android.architecture.blueprints.todoapp.data.Task
 
-import java.util.List;
+import java.util.List
 
 /**
  * Contains {@link BindingAdapter}s for the {@link Task} list.
  */
-public class TasksListBindings {
+object TasksListBindings {
 
     @SuppressWarnings("unchecked")
     @BindingAdapter("app:items")
-    public static void setItems(ListView listView, List<Task> items) {
-        TasksAdapter adapter = (TasksAdapter) listView.getAdapter();
-        if (adapter != null)
-        {
-            adapter.replaceData(items);
+    @JvmStatic fun setItems(listView: ListView, items: List<Task>) {
+        listView.adapter?.let {
+            (it as TasksAdapter).replaceData(items)
         }
     }
 }
