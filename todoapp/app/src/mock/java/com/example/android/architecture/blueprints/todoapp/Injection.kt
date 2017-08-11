f4ee88dbb33bb79ca12f81766147b88b3d749b3b
Yongwoo Lee <whylee259@gmail.com>
2017-08-11 17:11:42 +0900

    Converts a java class `Injection` to kotlin.

--- todoapp/app/src/mock/java/com/example/android/architecture/blueprints/todoapp/Injection.kt
+++ todoapp/app/src/mock/java/com/example/android/architecture/blueprints/todoapp/Injection.kt
@@ -14,28 +14,24 @@
  * limitations under the License.
  */
 
-package com.example.android.architecture.blueprints.todoapp;
+package com.example.android.architecture.blueprints.todoapp
 
-import android.content.Context;
-import android.support.annotation.NonNull;
+import android.content.Context
 
-import com.example.android.architecture.blueprints.todoapp.data.FakeTasksRemoteDataSource;
-import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource;
-import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository;
-import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource;
+import com.example.android.architecture.blueprints.todoapp.data.FakeTasksRemoteDataSource
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
 
-import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Enables injection of mock implementations for
  * {@link TasksDataSource} at compile time. This is useful for testing, since it allows us to use
  * a fake instance of the class to isolate the dependencies and run a test hermetically.
  */
-public class Injection {
+object Injection {
 
-    public static TasksRepository provideTasksRepository(@NonNull Context context) {
-        checkNotNull(context);
+    @JvmStatic fun provideTasksRepository(context: Context): TasksRepository {
         return TasksRepository.getInstance(FakeTasksRemoteDataSource.getInstance(),
-                TasksLocalDataSource.getInstance(context));
+                TasksLocalDataSource.getInstance(context))
     }
 }
